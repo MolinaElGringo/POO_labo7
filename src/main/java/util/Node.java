d31e3510d964bdf7e7365050b4ae60676a48bb83
Sebastian <diazsebas99@gmail.com>
2023-11-16 13:58:08 +0100

    implémentation de la node

--- src/main/java/util/Node.java
+++ src/main/java/util/Node.java
@@ -1,5 +1,32 @@
 package util;
 
 class Node<T> {
-    
+    private T value;
+    private Node<T> next;
+
+    public Node(T value){
+        this.value = value;
+
+    }
+
+    public Node(T value, Node<T> next){
+        this(value);
+        this.next = next;
+    }
+
+    public void setValue(T value){
+        this.value = value;
+    }
+
+    public void setNext(Node<T> node){
+        next = node;
+    }
+
+    public T getValue() {
+        return value;
+    }
+
+    public Node<T> getNext(){
+        return next;
+    }
 }
