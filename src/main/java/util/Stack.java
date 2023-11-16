91433321b5550da502de0453843afa77ab1927cd
GuillaumeDnt2 <guillaume.dunant@heig-vd.ch>
2023-11-16 14:04:15 +0100

    push and pop

--- src/main/java/util/Stack.java
+++ src/main/java/util/Stack.java
@@ -1,5 +1,33 @@
 package util;
 
-public class Stack {
+public class Stack<T> {
+
+    Node<T> head;
+
+    public Stack(){
+        head = null;
+    }
     
+    public void push(T value){
+        if (head == null) {
+            head = new Node<T>(value);
+        }
+        else{
+            head = new Node<T>(value, head);
+        }
+    }
+
+    public T pop(){
+        Node<T> top = head;
+        head = head.getPrevious();
+        return top.getValue();
+    }
+
+    public String toString(){
+        return null;
+    }
+
+    public T[] getValues(){
+        return null;
+    }
 }
