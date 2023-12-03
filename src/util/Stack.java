c9d18422d13474812d2a46c2d79bf5e84740761f
GuillaumeDnt2 <guillaume.dunant@heig-vd.ch>
2023-12-03 18:51:07 +0100

    Commentaires

--- src/util/Stack.java
+++ src/util/Stack.java
@@ -1,22 +1,41 @@
+/**
+ * @author  Sebastian Diaz & Guillaume Dunant
+ * Date   : 16.11.2023
+ * Fichier: Stack.java
+ */
 package util;
 
 import java.lang.reflect.Array;
 import java.util.Arrays;
 
+/**
+ * Implémentation de la pile
+ */
 public class Stack<T> {
 
     Node<T> head;
     int size = 0;
 
+    /**
+     * Constructeur
+     */
     public Stack(){
         head = null;
     }
 
+    /**
+     * Retourne un itérateur pour parcourir la stack
+     * @return Iterator<T>
+     */
     public Iterator<T> getIterator(){
         Iterator<T> ite = new Iterator<>(head);
         return ite;
     }
     
+    /**
+     * Ajoute une valeur sur la pile
+     * @param value Valeur à ajouter
+     */
     public void push(T value){
         if (head == null) {
             head = new Node<T>(value);
@@ -27,6 +46,10 @@ public class Stack<T> {
         ++size;
     }
 
+    /**
+     * Retourne et retire la dernière valeur de la stack
+     * @return La valeur retirée
+     */
     public T pop(){
 
         if(head == null){
@@ -38,10 +61,17 @@ public class Stack<T> {
         return top.getValue();
     }
 
+    /**
+     * Retourne les valeurs de la stacks en String
+     */
     public String toString(){
         return Arrays.toString(getValues());
     }
 
+    /**
+     * Retourne un tableau contenant les valeurs de la stacks
+     * @return T[] ou null si la stack est vide
+     */
     public T[] getValues(){
 
         if(head == null){
@@ -57,10 +87,17 @@ public class Stack<T> {
         return values;
     }
 
+    /**
+     * Retourne le nombre d'élément de la stack
+     * @return int
+     */
     public int size(){
         return size;
     }
 
+    /**
+     * Retire tous les éléments de la stack
+     */
     public void emptyStack(){
         head = null;
         size = 0;
