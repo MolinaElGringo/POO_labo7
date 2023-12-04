04f32f019575046ef395d0cc8701cfc5a7ff9b8f
GuillaumeDnt2 <guillaume.dunant@heig-vd.ch>
2023-12-04 20:30:19 +0100

    Ajout commentaires

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,20 @@
+/**
+ * @author  Sebastian Diaz & Guillaume Dunant
+ * Date   : 16.11.2023
+ * Fichier: Main.java
+ */
 import calculator.JCalculator;
-import util.*;
 
-
+/**
+ * Programme principale
+ */
 public class Main
 {
+  /**
+   * Démarre l'interface graphique
+   * @param args Arguments du programme
+   */
   public static void main(String ... args) {
     new JCalculator();
-    /* 
-    Stack<Integer> stk = new Stack<>();
-    stk.push(1);
-    stk.push(2);
-    stk.push(3);
-    System.out.println(stk);
-    stk.pop();
-    System.out.println(stk);
-    
-    for(int i = 4; i < 10 ; ++i){
-      stk.push(i);
-    }
-    System.out.println(stk);
-    var ite = stk.getIterator();
-    while (ite.hasNext()) {
-      System.out.println(ite.next());
-    }
-    */
   }
 }
